2358fd8fa529c0d1ca6706c7dde05d8f7a55e2e6
Rachel <xhuang1992@gmail.com>
2017-04-03 21:21:07 -0400

    Added Order DB connection

--- EEPLeafDataAccessLayer/src/DataAccessBase.java
+++ EEPLeafDataAccessLayer/src/DataAccessBase.java
@@ -73,4 +73,38 @@ public class DataAccessBase {
         log = output.toString();
         return DBConn;
     }
+    
+    protected Connection ConnectToOrderInfoDb(String log) {
+        Connection DBConn = null;           // MySQL connection handle
+        String errString = null;            // String for displaying errors
+        String msgString = null;            // String for displaying non-error messages
+        StringBuilder output = new StringBuilder();
+
+        // Connect to the inventory database
+        try {
+            msgString = ">> Establishing Driver...";
+            output.append("\n" + msgString);
+
+            //Load J Connector for MySQL - explicit loads are not needed for 
+            //connectors that are version 4 and better
+            //Class.forName( "com.mysql.jdbc.Driver" );
+            msgString = ">> Setting up URL...";
+            output.append("\n" + msgString);
+
+            //define the data source
+            String sourceURL = "jdbc:mysql://" + sqlServerIP + ":3306/orderinfo";
+
+            msgString = ">> Establishing connection with: " + sourceURL + "...";
+            output.append("\n" + msgString);
+
+            //create a connection to the db
+            DBConn = DriverManager.getConnection(sourceURL, sqlServerUserName, sqlServerPassword);
+
+        } catch (Exception e) {
+            errString = "\nProblem connecting to database:: " + e;
+            output.append(errString);
+        }
+        log = output.toString();
+        return DBConn;
+    }
 }
