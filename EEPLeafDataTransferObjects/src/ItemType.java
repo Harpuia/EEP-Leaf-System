2709d9dfafc1a5d947c2f492f0d7fd0ed6741b28
Rachel <xhuang1992@gmail.com>
2017-04-08 01:45:16 -0400

    Formated the code.

--- EEPLeafDataTransferObjects/src/ItemType.java
+++ EEPLeafDataTransferObjects/src/ItemType.java
@@ -1,16 +1,17 @@
+
 /**
  * ****************************************************************************
  *
- * This class enumerates all item types. 
+ * This class enumerates all item types.
  *
  *****************************************************************************
  */
 /**
-
-/**
+ *
+ * /**
  *
  * @author yazid
  */
-public enum ItemType{
+public enum ItemType {
     SEEDS, SHRUBS, TREES, CULTUREBOXES, GENOMICS, PROCESSING, REFERENCE_MATERIALS
 }
