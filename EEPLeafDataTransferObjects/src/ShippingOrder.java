173fd84f7d6b5a42c7ab4e647cad4a4c937c53cc
Rachel <xhuang1992@gmail.com>
2017-04-08 00:58:46 -0400

    Added comments.

--- EEPLeafDataTransferObjects/src/ShippingOrder.java
+++ EEPLeafDataTransferObjects/src/ShippingOrder.java
@@ -1,9 +1,14 @@
 
 import java.util.List;
-
+/**
+ * ****************************************************************************
+ *
+ * This class defines a DTO object for retrieving shipping order.
+ *
+ *****************************************************************************
+ */
 
 /**
- * DTO object for retrieving shipping order
  * @author Jiawei Li
  */
 public class ShippingOrder {
@@ -21,8 +26,10 @@ public class ShippingOrder {
     // the name of database table that stores order detail
     private String orderTable;
     
-    //private List<
-
+    
+    /**
+     * This constructor initiates the properties of a specific order.
+     */
     public ShippingOrder(int orderId, String orderDate, String firstName, String lastName, String address, String phoneNum, double totalCost, int shippedStatus, String orderTable) {
         this.orderId = orderId;
         this.orderDate = orderDate;
@@ -34,6 +41,9 @@ public class ShippingOrder {
         this.shippedStatus = shippedStatus;
         this.orderTable = orderTable;
     }
+    /**
+     * This constructor initiates the properties of a specific order with different variable types.
+     */
     public ShippingOrder(String orderId, String orderDate, String firstName, String lastName, String address, String phoneNum, String totalCost, String shippedStatus, String orderTable) {
         this.orderId = Integer.valueOf(orderId);
         this.orderDate = orderDate;
@@ -46,8 +56,8 @@ public class ShippingOrder {
         this.orderTable = orderTable;
     }
     /**
-     * determine whether this order has been shipped
-     * @return 
+     * Determine whether this order has been shipped
+     * @return boolean value to see if this order is shipped or not.
      */
     public boolean isShipped(){
         if(shippedStatus == 1){
@@ -56,12 +66,20 @@ public class ShippingOrder {
             return false;
         }
     }
+    
+    /**
+     * Mark the order as shipped.
+     */
     public void markShipped(){
         if(shippedStatus == 0){
             shippedStatus = 1;
         }
     }
     
+    /**
+     * Get the order table name.
+     * @return the order table name.
+     */
     public String getOrderTableName() {
         return orderTable;
     }
